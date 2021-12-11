548fd59a922b25dd6081a6abee154d0a2a539a7c
Artem Skripkin <skripkin.ae@icloud.com>
2021-12-11 15:44:12 +0400

    fixed code using pointers

--- lab4/lab4/main.cpp
+++ lab4/lab4/main.cpp
@@ -12,7 +12,7 @@ int main() {
     int n;
     cout << "enter values count: ";
     cin >> n;
-    int *array = new int[n];
+    float *array = new float[n];
     
     //filling array
     for (int counter = 0; counter < n; counter ++) {
@@ -21,23 +21,22 @@ int main() {
     }
     
     //finding min value in array and his index
-    int min = array[0];
-    int indexOfMin = 0;
+    float *minPointer;
+    minPointer = &array[0];
     float sum = 0;
     for (int counter = 0; counter < n; counter ++) {
         sum += array[counter];
-        if (array[counter] < min) {
-            min = array[counter];
-            indexOfMin = counter;
+        if (array[counter] < *minPointer) {
+            minPointer = &array[counter];
         }
     }
     
-    //printing min value and his index
-    cout << "min value (" << min << ") in array has index " << indexOfMin << "\n";
+    //printing min value, his index and adress
+    cout << "min value (" << *minPointer << ") in adress: " << minPointer << "\n";
     cout << "среднее арифмитическое всех чисел: " << sum / n << "\n";
     
     //replace min value in his index with arithmetical average of all values
-    array[indexOfMin] = sum / n;
-    cout << "value in index " << indexOfMin << " now is: " << array[indexOfMin] << "\n";
+    *minPointer = sum /n;
+    cout << "value in adress " << minPointer << " now is: " << *minPointer << "\n";
 }
 
