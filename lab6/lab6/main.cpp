48c2a9adf1c864a79d94822ea5d16aea4e13ca6f
Artem Skripkin <skripkin.ae@icloud.com>
2021-12-11 18:22:40 +0400

    initial commit

---
+++ lab6/lab6/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab6
+//
+//  Created by Артём Скрипкин on 11.12.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
