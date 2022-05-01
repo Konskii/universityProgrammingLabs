66924c4a20a00929dc7a4f51a66b703cea174782
Skripkin <artem.skriplkin@magenta-technology.com>
2022-05-01 14:58:28 +0400

    задание 1 выполнено
    Текст задания:
    Исходный текстовый файл “data.txt” находится в пап- ке Temp на диске C: и содержит целые числа. Переписать положи- тельные числа в текстовый файл “positive.txt” с тем же местоположением. При выполнении задания сохранить строгую структуру строк заданного файла, т.е. в конечном файле количество строк и их поря- док должны повторять исходный.

--- lab-2/lab-2/main.cpp
+++ lab-2/lab-2/main.cpp
@@ -7,7 +7,45 @@
 
 #include <iostream>
 #include <fstream>
+#include <string.h>
+using namespace std;
 
 int main() {
-
+    // MARK: - Задание 1
+    fstream dataStream;
+    fstream positiveStream;
+    try {
+        dataStream.open(
+                        "/Users/artem/Projects/universityProgrammingLabs/lab-2/Temp/data.txt",
+                        ios::in
+                        );
+    } catch(...) {
+        cout << "Ошибка открытия файла data.txt" << endl;
+        return 1;
+    }
+    try {
+        positiveStream.open(
+                            "/Users/artem/Projects/universityProgrammingLabs/lab-2/Temp/positive.txt",
+                            ios::out
+                            );
+    } catch (...) {
+        cout << "Ошибка открытия файла positive.txt" << endl;
+        return 2;
+    }
+    int a;
+    while (!dataStream.eof()) {
+        dataStream >> a;
+        if (a > 0) {
+            positiveStream << a;
+        }
+        positiveStream << "\n";
+    }
+    
+    positiveStream.open(
+                        "/Users/artem/Projects/universityProgrammingLabs/lab-2/Temp/positive.txt",
+                        ios::out
+                        );
+    dataStream.close();
+    positiveStream.close();
+    return 0;
 }
