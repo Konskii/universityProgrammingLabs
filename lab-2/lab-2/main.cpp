e11b9f7b5f33e0876a078c9078aa456bd80dd1ad
Skripkin <artem.skriplkin@magenta-technology.com>
2022-05-01 16:08:34 +0400

    задание 2 выполнено
    Текст задания:
    Разработать программу, которая определяет и записывает в текстовый файл характеристики основных типов данных в С++: char, short int, unsigned short int, int, unsigned int, long int, unsigned long int, float, double, long double.
    Характеристики:
    1) число байт, отводимое под тип данных;
    2) минимальное значения, которое может хранить указанный
    тип данных;
    3) максимальное значения, которое может хранить указанный
    тип данных.

--- lab-2/lab-2/main.cpp
+++ lab-2/lab-2/main.cpp
@@ -8,6 +8,8 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
+#include <math.h>
+#include <limits>
 using namespace std;
 
 int main() {
@@ -47,5 +49,75 @@ int main() {
                         );
     dataStream.close();
     positiveStream.close();
+    
+    //MARK: - Задание 2
+    fstream dataTypesStream;
+    try {
+        dataTypesStream.open(
+                             "/Users/artem/Projects/universityProgrammingLabs/lab-2/Temp/dataTypes.txt",
+                             ios::out
+                             );
+    } catch (...) {
+        cout << "Ошибка открытия файла dataTypes.txt" << endl;
+        return 3;
+    }
+    
+    //Заголовки
+    dataTypesStream << setiosflags(ios::left) << setw(25) << "Type:";
+    dataTypesStream << setw(15) << "Size:";
+    dataTypesStream << setw(25) << "Max value:";
+    dataTypesStream << setw(40) << "Min value:" << endl;
+    //char
+    dataTypesStream << setiosflags(ios::left) << setw(25) << "char";
+    dataTypesStream << setw(15) << sizeof(char);
+    dataTypesStream << setw(25) << CHAR_MAX;
+    dataTypesStream << setw(40) << CHAR_MIN << endl;
+    //short int
+    dataTypesStream << setiosflags(ios::left) << setw(25) << "short int";
+    dataTypesStream << setw(15) << sizeof(short int);
+    dataTypesStream << setw(25) << numeric_limits<short int>::max();
+    dataTypesStream << setw(40) << numeric_limits<short int>::min() << endl;
+    //unsigned short int
+    dataTypesStream << setiosflags(ios::left) << setw(25) << "unsigned short int";
+    dataTypesStream << setw(15) << sizeof(unsigned short int);
+    dataTypesStream << setw(25) << numeric_limits<unsigned short int>::max();
+    dataTypesStream << setw(40) << numeric_limits<unsigned short int>::min() << endl;
+    //int
+    dataTypesStream << setiosflags(ios::left) << setw(25) << "int";
+    dataTypesStream << setw(15) << sizeof(int);
+    dataTypesStream << setw(25) << numeric_limits<int>::max();
+    dataTypesStream << setw(40) << numeric_limits<int>::min() << endl;
+    //unsigned int
+    dataTypesStream << setiosflags(ios::left) << setw(25) << "unsigned int";
+    dataTypesStream << setw(15) << sizeof(unsigned int);
+    dataTypesStream << setw(25) << numeric_limits<unsigned int>::max();
+    dataTypesStream << setw(40) << numeric_limits<unsigned int>::min() << endl;
+    //long int
+    dataTypesStream << setiosflags(ios::left) << setw(25) << "long int";
+    dataTypesStream << setw(15) << sizeof(long int);
+    dataTypesStream << setw(25) << numeric_limits<long int>::max();
+    dataTypesStream << setw(40) << numeric_limits<long int>::min() << endl;
+    //unsigned long int
+    dataTypesStream << setiosflags(ios::left) << setw(25) << "unsigned long int";
+    dataTypesStream << setw(15) << sizeof(unsigned long int);
+    dataTypesStream << setw(25) << numeric_limits<unsigned long int>::max();
+    dataTypesStream << setw(40) << numeric_limits<unsigned long int>::min() << endl;
+    //float
+    dataTypesStream << setiosflags(ios::left) << setw(25) << "float";
+    dataTypesStream << setw(15) << sizeof(float);
+    dataTypesStream << setw(25) << numeric_limits<float>::max();
+    dataTypesStream << setw(40) << numeric_limits<float>::min() << endl;
+    //double
+    dataTypesStream << setiosflags(ios::left) << setw(25) << "double";
+    dataTypesStream << setw(15) << sizeof(double);
+    dataTypesStream << setw(25) << numeric_limits<double>::max();
+    dataTypesStream << setw(40) << numeric_limits<double>::min() << endl;
+    //long double
+    dataTypesStream << setiosflags(ios::left) << setw(25) << "long double";
+    dataTypesStream << setw(15) << sizeof(long double);
+    dataTypesStream << setw(25) << numeric_limits<long double>::max();
+    dataTypesStream << setw(40) << numeric_limits<long double>::min();
+    
+    dataTypesStream.close();
     return 0;
 }
