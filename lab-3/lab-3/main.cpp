543a85b7044401c5a1b9d0e3019d07ea24b0d96c
Skripkin <artem.skriplkin@magenta-technology.com>
2022-05-03 12:56:47 +0400

    Задание 1 выполнено
    Текст задания:
    В текстовом файле «STUDENTS.TXT» записана информация о студентах (файл считать корректным). Создать файл «GRANT.TXT», содержащий список студентов-первокурсников, полу- чающих государственную академическую стипендию в увеличенном размере (имеющих суммарный балл по ЕГЭ от 215 баллов и выше).

--- lab-3/lab-3/main.cpp
+++ lab-3/lab-3/main.cpp
@@ -6,7 +6,44 @@
 //
 
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main() {
+    // MARK: - Задание 1
+    fstream studentStream;
+    fstream grantStream;
+    try {
+        studentStream.open(
+                            "/Users/artem/Projects/universityProgrammingLabs/lab-3/STUDENT.txt",
+                           iostream::in
+                            );
+    } catch (...) {
+        cout << "Ошибка открытия файла STUDENT.txt" << endl;
+        return 1;
+    }
+    try {
+        grantStream.open(
+                            "/Users/artem/Projects/universityProgrammingLabs/lab-3/GRANT.txt",
+                           iostream::out
+                            );
+    } catch (...) {
+        cout << "Ошибка открытия/создания файла GRANT.txt" << endl;
+        return 2;
+    }
+    
+    while (!studentStream.eof()) {
+        string LastName;
+        string Name;
+        int first;
+        int second;
+        int third;
+        studentStream >> LastName;
+        studentStream >> Name;
+        studentStream >> first >> second >> third;
+        if (first + second + third >= 215 ) {
+            grantStream << LastName << " " << Name << " " << first << " " << second << " " << third << endl;
+        }
+    }
     return 0;
 }
