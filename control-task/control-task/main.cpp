1986078fc5d2f4389c6d94c34b30b53f49da4132
Skripkin <artem.skripkin@magenta-technology.com>
2023-06-23 10:23:41 +0400

    initial commit

---
+++ control-task/control-task/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  control-task
+//
+//  Created by Артём Скрипкин on 23.06.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
